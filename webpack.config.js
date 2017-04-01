c8836000c8b666bffef906e08324211f5d7e1aae
Chris Beard <coebe500@aol.com>
2017-04-01 10:50:48 +0100

    Build: NODE_ENV=production npm run clean && webpack -p. p for production to minify output. For environment, added webpack.DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,9 @@ module.exports = {
   	new HtmlWebpackPlugin({
   		// builds a default by itself or you can feed it a template like so:
   		template: 'src/index.html'
-  	})
+  	}),
+    new webpack.DefinePlugin({ // defines window scope variables that are used in JS output files. variable left, value right. JSON.stringify just makes sure it's JSON friendly
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) // webpack looks fro process.env.NODE_ENV on window scope. In production you can have error checks but live can be ignored for faster speed. Note that process.env.NODE_ENV can be set as a variable (for the right handside only.)
+    })
   ]
 };
